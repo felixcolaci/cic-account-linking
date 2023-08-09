343b346cf15c3a54fbc4c9f621c723914da8d3b1
Felix Colaci <felix.colaci@okta.com>
2023-08-09 18:03:37 +0200

    switch to ts

---
+++ functions/sign-gathered-data/sign-gathered-data.ts
@@ -0,0 +1,76 @@
+import { Handler } from "@netlify/functions";
+import { jwtVerify, SignJWT } from "jose";
+
+const { TOKEN_SIGNING } = process.env;
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
+const handleCors = (event) => {
+  if (event.httpMethod == "OPTIONS") {
+    // To enable CORS
+    return {
+      statusCode: 200, // <-- Important!
+      headers: corsHeaders,
+      body: "Options Callback",
+    };
+  }
+};
+
+const handleError = (message: string, status = 400) => {
+  return {
+    statusCode: status,
+    body: JSON.stringify({
+      message,
+      status,
+    }),
+    headers: {
+      "content-type": "application/json",
+      ...corsHeaders,
+    },
+  };
+};
+
+export const handler: Handler = async (event, context) => {
+  const corsResponse = handleCors(event);
+  if (corsResponse) {
+    return corsResponse;
+  }
+
+  // @ts-ignore
+  const { state, requiredData, sessionToken } = JSON.parse(event.body);
+  if (!state || !requiredData || !sessionToken) {
+    return handleError("provide state, token and payload!");
+  }
+
+  try {
+    const { payload } = await jwtVerify(sessionToken, Buffer.from(TOKEN_SIGNING || ""), {
+      algorithms: ["HS256"],
+    });
+
+    const token = await new SignJWT({
+      state,
+      requiredData,
+    })
+      .setSubject(payload.sub || "")
+      .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+      .setIssuedAt()
+      .setIssuer("https://auth0-progressive-profiling.netflify.app")
+      .setExpirationTime("60s")
+      .sign(Buffer.from(TOKEN_SIGNING || ""));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token }),
+      headers: {
+        "content-type": "application/json",
+        ...corsHeaders,
+      },
+    };
+  } catch (error) {
+    return handleError("the provided token is invalid", 401);
+  }
+};
---
+++ functions/verify-session/verify-session.ts
@@ -0,0 +1,62 @@
+import { Handler } from "@netlify/functions";
+import { jwtVerify } from "jose";
+
+const { TOKEN_SIGNING } = process.env;
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
+const handleError = (message: string, status = 400) => {
+  return {
+    statusCode: status,
+    body: JSON.stringify({
+      message,
+      status,
+    }),
+    headers: {
+      "content-type": "application/json",
+      ...corsHeaders,
+    },
+  };
+};
+const handleCors = (event) => {
+  if (event.httpMethod == "OPTIONS") {
+    // To enable CORS
+    return {
+      statusCode: 200, // <-- Important!
+      headers: corsHeaders,
+      body: "Options Callback",
+    };
+  }
+};
+
+export const handler: Handler = async (event, context) => {
+  const corsResponse = handleCors(event);
+  if (corsResponse) {
+    return corsResponse;
+  }
+  //@ts-ignore
+  const { sessionToken } = JSON.parse(event.body);
+  if (!sessionToken) {
+    return handleError("no token provided");
+  }
+
+  try {
+    const { payload } = await jwtVerify(sessionToken, Buffer.from(TOKEN_SIGNING || ""), {
+      algorithms: ["HS256"],
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(payload),
+      headers: {
+        "content-type": "application/json",
+        ...corsHeaders,
+      },
+    };
+  } catch (error) {
+    return handleError("the provided token is invalid", 401);
+  }
+};
