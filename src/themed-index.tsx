bd27348c89285cd7c443cf47ed0cb8d2998c706a
Felix Colaci <felix.colaci@okta.com>
2023-08-14 11:09:12 +0200

    theming

--- src/themed-index.tsx
+++ src/themed-index.tsx
@@ -2,23 +2,22 @@ import { NextUIProvider, changeTheme } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { useBrandingStore } from "./misc/branding.store";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getTheme } from "./misc/theme";
 
 export const ThemedIndex = () => {
   const branding = useBrandingStore();
 
-  const [theme, setTheme] = useState<any>();
+  // const [theme, setTheme] = useState<any>();
 
   useEffect(() => {
     const newTheme = getTheme(branding);
-    setTheme(newTheme);
     changeTheme(newTheme);
     console.log("root", branding);
   }, [branding]);
 
   return (
-    <NextUIProvider theme={theme}>
+    <NextUIProvider theme={getTheme(branding)}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
