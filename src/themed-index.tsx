75b243b2393de35042368dfbe0938064ba613508
Felix Colaci <felix.colaci@okta.com>
2023-08-21 11:40:18 +0200

    disable linting

--- src/themed-index.tsx
+++ src/themed-index.tsx
@@ -20,6 +20,7 @@ export const ThemedIndex = () => {
       setTheme(getTheme(localBranding));
       searchParams.delete("branding");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
